03c55c5c24cd8ae7b1abd64ca2da7e89351b2dd3
Kristina <Kristina@172.16.213.96>
2022-12-02 12:55:38 -0500

    2

--- src/main/java/com/mycompany/ejercicio03_02/Ventana1.java
+++ src/main/java/com/mycompany/ejercicio03_02/Ventana1.java
@@ -5,7 +5,14 @@
 package com.mycompany.ejercicio03_02;
 
 import java.awt.HeadlessException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 /**
  *
@@ -13,15 +20,37 @@ import javax.swing.JFrame;
  */
 public class Ventana1 extends JFrame {
 
+    private List<JPanel> jPanelList;
+    private List<JLabel> jLabelList;
+    private List<JTextField> jTextFieldList;
+    private List<JComboBox> jComboBox;
+    private List<JButton> jButton;
+
     public Ventana1(String title) throws HeadlessException {
         super(title);
-        this.setSize(600,400);
+        this.setSize(600, 400);
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setContentPane(rootPane);
         this.setVisible(true);
+
+    }
+    
+    private void iniciarComponentes(){
         
         
     }
-    
+    private void crearPaneles(){
+        
+        jPanelList = new ArrayList<>();
+        jPanelList.add(new JPanel());
+        jPanelList.add(new JPanel());
+        jPanelList.add(new JPanel());
+        jPanelList.add(new JPanel());
+        jPanelList.add(new JPanel());
+        jPanelList.add(new JPanel());
+        jPanelList.add(new JPanel());
+        
+    }
+
 }
