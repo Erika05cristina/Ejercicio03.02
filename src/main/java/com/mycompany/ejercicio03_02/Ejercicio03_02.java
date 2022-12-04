7b78562be1b8672fa4e6625e3b1293c4f319f174
Kristina <Kristina@172.16.213.96>
2022-12-04 13:13:26 -0500

    6

--- src/main/java/com/mycompany/ejercicio03_02/Ejercicio03_02.java
+++ src/main/java/com/mycompany/ejercicio03_02/Ejercicio03_02.java
@@ -12,7 +12,10 @@ public class Ejercicio03_02 {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        var ventana1 = new Ventana1(" Ventana 1 ");
-        var ventana5 = new Ventana5(" Ventana 5 ");
+       // var ventana1 = new Inscribete(" Inscribete ");
+        //var ventana2 = new VentanaDos(); 
+        //var ventana3 = new InfoCarrera();
+        var ventana4 = new CrearGrupo();  
+        //var ventana5 = new Ventana5(" Ventana 5 ");
     }
 }
